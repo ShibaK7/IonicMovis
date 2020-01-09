fe62e15bf24be8991a7d47114cd4619834342a42
ShibaK7 <shirou.k7@gmail.com>
2020-01-09 15:01:34 -0600

    Crear tarjetas con botones

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,48 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  cardsData = [
+    {
+      title: 'Titulo 1', 
+      subtitle: 'Subtitulo 1',
+      contentText:'Texto 1'    
+    },
+    {
+      title: 'Titulo 2', 
+      subtitle: 'Subtitulo 2',
+      contentText:'Texto 2'    
+    },
+    {
+      title: 'Titulo 3', 
+      subtitle: 'Subtitulo 3',
+      contentText:'Texto 3'    
+    }
+  ];
 
+  constructor(private alertController: AlertController) { }
+
+  onBeerClicked(){
+    console.log("Quiero una no tan fría");
+  }
+
+  openAlert() {
+    this.alertController.create({
+        header: 'Hello world!',
+        subHeader: '¡Los baños estan mas limpios ahora!',
+        buttons: [
+            {
+                text: 'Cerrar',
+                handler: () => {
+                    console.log('bye bye!');
+                }
+            }
+        ]
+    }).then(alert => {
+        return alert.present();
+    }).then(() => {
+        console.log('ya se terminó de abrir 100% seguro');
+    });
+
+}
+  
 }
